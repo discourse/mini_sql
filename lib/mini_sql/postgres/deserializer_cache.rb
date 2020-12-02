edcf90e212c8d79c32355f1a2768bf61036bad75
Sergey Malykh <xronos.i.am@gmail.com>
2020-12-02 17:56:52 +0300

    serializable materializer

--- lib/mini_sql/postgres/deserializer_cache.rb
+++ lib/mini_sql/postgres/deserializer_cache.rb
@@ -4,6 +4,85 @@ module MiniSql
   module Postgres
     class DeserializerCache
 
+      class SerializableMaterializer < Array
+        attr_accessor :materializer, :decorator_module
+
+        def initialize(materializer:, decorator_module: nil)
+          @materializer = materializer
+          @decorator_module = decorator_module
+        end
+
+        def self.build_materializer(fields:)
+          i = 0
+          while i < fields.length
+            # special handling for unamed column
+            if fields[i] == "?column?"
+              fields[i] = "column#{i}"
+            end
+            i += 1
+          end
+
+          Class.new do
+            attr_accessor(*fields)
+
+            # AM serializer support
+            alias :read_attribute_for_serialization :send
+
+            def to_h
+              r = {}
+              instance_variables.each do |f|
+                r[f.to_s.sub('@', '').to_sym] = instance_variable_get(f)
+              end
+              r
+            end
+
+            def self.materialize_hash(result_hash)
+              r = self.new
+              result_hash.each do |field, value|
+                r.send(:"#{field}=", value)
+              end
+              r
+            end
+
+            instance_eval <<~RUBY
+              def materialize(pg_result, index)
+                r = self.new
+                #{col = -1; fields.map { |f| "r.#{f} = pg_result.getvalue(index, #{col += 1})" }.join("; ")}
+                r
+              end
+            RUBY
+          end
+        end
+
+        def materialize(result)
+          i = 0
+          r = []
+          # quicker loop
+          while i < result.ntuples
+            r << materializer.materialize(result, i)
+            i += 1
+          end
+          replace(r)
+        end
+
+        def marshal_dump
+          {
+            result: map(&:to_h),
+            decorator_module: decorator_module,
+          }
+        end
+
+        def marshal_load(result:, decorator_module:)
+          materializer = SerializableMaterializer.build_materializer(fields: result[0].keys)
+          materializer.include(decorator_module) if decorator_module
+          replace(
+            result.map do |deserialized_result|
+              materializer.materialize_hash(deserialized_result)
+            end
+          )
+        end
+      end
+
       DEFAULT_MAX_SIZE = 500
 
       def initialize(max_size = nil)
@@ -18,7 +97,7 @@ module MiniSql
         if materializer
           @cache[key] = materializer
         else
-          materializer = @cache[key] = new_row_matrializer(result)
+          materializer = @cache[key] = SerializableMaterializer.build_materializer(fields: result.fields)
           @cache.shift if @cache.length > @max_size
         end
 
@@ -28,66 +107,15 @@ module MiniSql
       def materialize(result, decorator_module = nil)
         return [] if result.ntuples == 0
 
-        key = result.fields
+        cached_materializer = materializer(result)
+        cached_materializer.include(decorator_module) if decorator_module
 
-        # trivial fast LRU implementation
-        materializer = @cache.delete(key)
-        if materializer
-          @cache[key] = materializer
-        else
-          materializer = @cache[key] = new_row_matrializer(result)
-          @cache.shift if @cache.length > @max_size
-        end
-
-        materializer.include(decorator_module) if decorator_module
-
-        i = 0
-        r = []
-        # quicker loop
-        while i < result.ntuples
-          r << materializer.materialize(result, i)
-          i += 1
-        end
-        r
-      end
-
-      private
-
-      def new_row_matrializer(result)
-        fields = result.fields
-
-        i = 0
-        while i < fields.length
-          # special handling for unamed column
-          if fields[i] == "?column?"
-            fields[i] = "column#{i}"
-          end
-          i += 1
-        end
-
-        Class.new do
-          attr_accessor(*fields)
-
-          # AM serializer support
-          alias :read_attribute_for_serialization :send
-
-          def to_h
-            r = {}
-            instance_variables.each do |f|
-              r[f.to_s.sub('@', '').to_sym] = instance_variable_get(f)
-            end
-            r
-          end
-
-          instance_eval <<~RUBY
-            def materialize(pg_result, index)
-              r = self.new
-              #{col = -1; fields.map { |f| "r.#{f} = pg_result.getvalue(index, #{col += 1})" }.join("; ")}
-              r
-            end
-          RUBY
-        end
+        SerializableMaterializer.new(
+          materializer: cached_materializer,
+          decorator_module: decorator_module
+        ).materialize(result)
       end
     end
+
   end
 end
