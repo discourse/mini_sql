f77abcbf6d144314f34ee6cbf4fcfe9785c41141
Sergey Malykh <xronos.i.am@gmail.com>
2020-12-30 01:16:34 +0300

    FEATURE: Compacted JSON serialization (#24)

---
+++ lib/mini_sql/result.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module MiniSql
+  module Result
+    # AM serializer support
+    alias :read_attribute_for_serialization :send
+
+    def to_h
+      r = {}
+      instance_variables.each do |f|
+        r[f.to_s.delete_prefix('@').to_sym] = instance_variable_get(f)
+      end
+      r
+    end
+
+    def values
+      instance_variables.map { |f| instance_variable_get(f) }
+    end
+  end
+end
