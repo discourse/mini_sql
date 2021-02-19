95d163c79c73253b21b515719202fffa554e857f
Sergei Malykh <xronos.i.am@gmail.com>
2021-02-19 16:51:38 +0300

    results equality

--- lib/mini_sql/result.rb
+++ lib/mini_sql/result.rb
@@ -16,5 +16,15 @@ module MiniSql
     def values
       instance_variables.map { |f| instance_variable_get(f) }
     end
+
+    def ==(other_result)
+      self.class.decorator == other_result.class.decorator && 
+      self.instance_variables == other_result.instance_variables &&
+      self.values == other_result.values
+    end
+
+    def eql?(other_result)
+      self == other_result
+    end
   end
 end
